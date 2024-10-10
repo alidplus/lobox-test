c36ff8affdfd254ed45300c23f37bedc2ba0079d
Ali Ghorbani <alidoubleplus@gmail.com>
2024-10-10 17:31:20 +0330

    fix icon attributes

--- src/icon/lib/check.tsx
+++ src/icon/lib/check.tsx
@@ -12,7 +12,7 @@ export default function CheckIcon(props: IconProps) {
 
   return (
     <svg fill="none" width={width} height={height} viewBox="0 0 24 24">
-      <path d="M4 12.6111L8.92308 17.5L20 6.5" stroke={fill} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M4 12.6111L8.92308 17.5L20 6.5" stroke={fill} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
   )
 }
--- src/icon/lib/chevron-down.tsx
+++ src/icon/lib/chevron-down.tsx
@@ -12,7 +12,7 @@ export default function ChevronDownIcon(props: IconProps) {
 
   return (
     <svg fill="none" width={width} height={height} viewBox="0 0 24 24">
-      <path d="M6 9L11.7874 14.7874V14.7874C11.9048 14.9048 12.0952 14.9048 12.2126 14.7874V14.7874L18 9" stroke={fill} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M6 9L11.7874 14.7874V14.7874C11.9048 14.9048 12.0952 14.9048 12.2126 14.7874V14.7874L18 9" stroke={fill} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
   )
 }
